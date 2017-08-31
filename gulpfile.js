fc21374f765dce6f24e91ff865a2bc206d44ba56
Ankit Chaurasia <ankit.chaurasia@ggktech.local>
2017-08-31 18:59:39 +0530

    Updated gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,22 @@ const sass = require('gulp-ruby-sass');
 
 // Sass task
 gulp.task('sass', () =>
-	sass('sass/**/*.scss')
+	sass('sass/**/*.sass')
 		.on('error', sass.logError)
 		.pipe(gulp.dest('css/'))
 );
 
+// Scss task
+gulp.task('scss', () =>
+sass('scss/**/*.scss')
+    .on('error', sass.logError)
+    .pipe(gulp.dest('css/'))
+);
+
 // Watch task
 gulp.task('watch', function(){
-    gulp.watch('sass/**/*.scss', ['sass']);
+    gulp.watch('sass/**/*.sass', ['sass']);
+    gulp.watch('scss/**/*.scss', ['scss']);
 });
 
 // Default task
